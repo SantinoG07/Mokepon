33ff3f5494d98c0b9dae897ba0eb331bdb881854
santinog07 <santino.gennuso.m@gmail.com>
2026-01-01 18:21:47 -0300

    Agregamos mas datos variables, ademas de crear la seccion de combate y renombrar algunas variables, no funciona el printeo de los ataques

--- src/lib/script.js
+++ src/lib/script.js
@@ -1,8 +1,26 @@
+//Creacion de las variables
+let inputratigueya
+let inputcapipepo
+let inputhipodoge
+let mascenemigo
+let ataques
 
-function prueba(){
-let confirmarselec = document.getElementById('button-confirmar')
-let cardmokepon
-let Mokepones = []
+//Obtencion de los elementos (Getters)
+let confirmarmasc = document.getElementById('button-confirmar-masc')
+let opcionesmasc = document.getElementById('div-opc-mascotas')
+let sectioncombate = document.getElementById('section-combate')
+let divimagescombate = document.getElementById('div-images')
+let divvidas = document.getElementById('div-vidas')
+let divbuttons = document.getElementById('div-buttons')
+
+
+//Eventos de los getters
+confirmarmasc.addEventListener('click', confirmarseleccion)
+
+
+
+
+//Creacion de los mokepones
 class Mokepon {
     constructor(vida, nombre, foto, descripcion) {
         this.vida = vida
@@ -39,17 +57,25 @@ capipepo.ataques.push(
     { nombre: '🌱', id: 'boton-tierra' }
 )
 
+
+
+//Array de mokepones
+let Mokepones = []
 Mokepones.push(ratigueya, hipodoge, capipepo)
 
-let opcionesmasc = document.getElementById('opc-mascotas')
 
 
-function inicializarjuego(){
-    Mokepones.forEach((Mokepon)=>{
-        cardmokepon = `
+
+
+function cargarjuego() {
+
+    sectioncombate.style.display='none'
+
+    Mokepones.forEach((Mokepon) => {
+        let cardmokepon = `
         <div class="bg-black w-70 h-100 rounded-2xl shadow-2xl flex flex-col">
-        <label class='flex flex-1 flex-col'>
-        <input type="radio" class="hidden"/>
+        <label class='flex flex-1 flex-col' for=${Mokepon.nombre}>
+        <input type="radio" class="hidden" name="mokepones" id=${Mokepon.nombre} value=${Mokepon.nombre}/>
             <img src="${Mokepon.foto}"/>
             <h1 class="text-white text-center mt-10">${Mokepon.nombre}</h1>
             <h3 class='text-white m-4'>${Mokepon.descripcion}</h3>
@@ -58,22 +84,76 @@ function inicializarjuego(){
             </label>
         </div>
         `
-    //El map lo que hace es un array temporal el cual filtra segun la condicion que le pongamos, el join separa con un espacio cada elemento
-
-        opcionesmasc.innerHTML+=cardmokepon
+        //El map lo que hace es un array temporal el cual filtra segun la condicion que le pongamos, el join separa con un espacio cada elemento
+        
+        
+        opcionesmasc.innerHTML += cardmokepon
 
     })
-    
-    
-}
-inicializarjuego()
-confirmarselec.addEventListener('click',ocultar)
+
+        inputratigueya = document.getElementById('Ratigueya')
+        inputcapipepo = document.getElementById('Capipepo')
+        inputhipodoge = document.getElementById('Hipodoge')
 
 }
 
-function ocultar(){
+function confirmarseleccion() {
+    sectioncombate.style.display='block'
     let sectionselec = document.getElementById('section-selec-masc')
-    sectionselec.style.display='none'
+    sectionselec.style.display = 'none'
+    selecmascenemigo()
+
+    if(inputratigueya.checked){
+        combate('ratigueya')
+    }else if(inputcapipepo.checked){
+        combate('capipepo')
+    }else if(inputhipodoge.checked){
+        combate('hipodoge')
+    }
 }
 
-window.addEventListener('load', prueba)
+function combate(nombre){
+
+    let imagenjugador = document.createElement('img')
+    imagenjugador.src = `/${nombre}.png`
+    imagenjugador.classList.add('w-32', 'h-auto')
+    imagenjugador.innerHTML = nombre
+
+    divimagescombate.appendChild(imagenjugador)
+
+    let imagenenemigo = document.createElement('img')
+    imagenenemigo.src = `/${mascenemigo}.png`
+    imagenenemigo.classList.add('w-32', 'h-auto')
+
+    imagenenemigo.innerHTML = nombre
+
+    divimagescombate.appendChild(imagenenemigo)
+
+    ataques.forEach(ataque =>{
+    let buttonsataque = `<button>${ataque.nombre}</button>`
+
+    divbuttons.innerHTML += buttonsataque
+    }
+
+    )
+
+
+
+}
+
+function selecmascenemigo(){
+    let numero = aleatorio(1,3)
+    if(numero==1){
+        mascenemigo = "ratigueya";
+    }else if(numero==2){
+        mascenemigo = "hipodoge"
+    }else {
+        mascenemigo = "capipepo"
+    }
+}
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+window.addEventListener('load', cargarjuego)
